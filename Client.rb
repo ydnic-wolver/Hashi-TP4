98a064b264d7b183f29f4d9581214eb27f386cf2
Nowyyy <59916891+Nowyyy@users.noreply.github.com>
2021-03-19 13:32:29 +0100

    Ajout du chronomètre

---
+++ Client.rb
@@ -0,0 +1,16 @@
+
+load 'MainMenu.rb'
+load "APropos.rb"
+load "Classement.rb"
+load "MenuSelection.rb"
+load "Tuto.rb"
+load 'BoutonChoix.rb'
+load "Pause.rb"
+load 'Plateau.rb'
+
+
+window = MainMenu.new
+
+#$window = Plateau.new
+
+Gtk.main
---
+++ Hashi.rb
@@ -0,0 +1,18 @@
+require 'gtk3'
+
+# Cette classe représente le plateau du jeu
+# c'est dans cette classe que toute la logique du jeu va
+#  s'articuler
+
+
+load "HashiGame.rb"
+
+
+hashi = HashiGame.new
+
+
+
+# Chargement d'une grille
+
+
+Gtk.main
